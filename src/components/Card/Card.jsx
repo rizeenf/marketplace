c60e47da5364a883ac59b25287fb92799e08e371
rizeenf <rize.nf11@gmail.com>
2023-08-10 20:54:26 +0700

    adding API

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,16 +8,22 @@ const Card = ({ item }) => {
         <div className="wrapper flex justify-center items-center flex-col gap-1 ">
           <figure className=" w-full h-96 overflow-hidden relative cursor-pointer">
             <img
-              src={item.img1}
-              alt={item.title}
+              src={
+                import.meta.env.VITE_APP_URL +
+                item?.attributes?.img1.data.attributes.url
+              }
+              alt={item?.title}
               className=" w-full h-full object-cover absolute z-20 hover:z-0"
             />
             <img
-              src={item.img2}
-              alt={item.title}
-              className=" w-full h-full object-cover absolute z-10"
+              src={
+                import.meta.env.VITE_APP_URL +
+                item?.attributes?.img2.data.attributes.url
+              }
+              alt={item?.attributes.title}
+              className=" w-full h-full object-cover absolute z-10 hover:z-20"
             />
-            {item.newItem ? (
+            {item?.attributes.newItem ? (
               <figcaption className="absolute top-1 right-1 bg-gray-400 text-white uppercase p-1 rounded-sm text-sm z-30">
                 New item
               </figcaption>
@@ -26,14 +32,14 @@ const Card = ({ item }) => {
             )}
           </figure>
           <div className="title self-start text-base cursor-pointer">
-            <h2>{item.title}</h2>
+            <h2>{item?.attributes.title}</h2>
           </div>
           <div className="price flex flex-row gap-3 self-start justify-center items-center">
             <span className="oldprice text-sm line-through text-gray-500 ">
-              IDR {item.oldPrice}K
+              IDR {item?.attributes.oldPrice}K
             </span>
             <span className="newprice text-lg font-semibold">
-              IDR {item.newPrice}K
+              IDR {item?.attributes.price}K
             </span>
           </div>
         </div>
--- src/components/Sections/Sections.jsx
+++ src/components/Sections/Sections.jsx
@@ -1,45 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../Card/Card.jsx";
+import axios from "axios";
 
 const Sections = ({ type }) => {
-  const items = [
-    {
-      id: 1,
-      title: "Peaky blinder's Coat",
-      img1: "https://images.pexels.com/photos/1206873/pexels-photo-1206873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/1643025/pexels-photo-1643025.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      oldPrice: 52,
-      newPrice: 45,
-      newItem: true,
-    },
-    {
-      id: 2,
-      title: "Red Woman's Coat",
-      img1: "https://images.pexels.com/photos/158648/girl-coat-old-coat-brown-coat-158648.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/1906877/pexels-photo-1906877.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
-      oldPrice: 42,
-      newPrice: 35,
-      newItem: false,
-    },
-    {
-      id: 3,
-      title: "Grey Woman's Coat",
-      img1: "https://images.pexels.com/photos/3538028/pexels-photo-3538028.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/2043592/pexels-photo-2043592.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      oldPrice: 33,
-      newPrice: 30,
-      newItem: false,
-    },
-    {
-      id: 4,
-      title: "Red Woman's Coat",
-      img1: "https://images.pexels.com/photos/3951783/pexels-photo-3951783.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/3951785/pexels-photo-3951785.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      oldPrice: 58,
-      newPrice: 49,
-      newItem: true,
-    },
-  ];
+  // const items = [
+  //   {
+  //     id: 1,
+  //     title: "Peaky blinder's Coat",
+  //     img1: "https://images.pexels.com/photos/1206873/pexels-photo-1206873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //     img2: "https://images.pexels.com/photos/1643025/pexels-photo-1643025.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  //     oldPrice: 52,
+  //     newPrice: 45,
+  //     newItem: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Red Woman's Coat",
+  //     img1: "https://images.pexels.com/photos/158648/girl-coat-old-coat-brown-coat-158648.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  //     img2: "https://images.pexels.com/photos/1906877/pexels-photo-1906877.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+  //     oldPrice: 42,
+  //     newPrice: 35,
+  //     newItem: false,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Grey Woman's Coat",config
+  //     newPrice: 30,
+  //     newItem: false,
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Red Woman's Coat",
+  //     img1: "https://images.pexels.com/photos/3951783/pexels-photo-3951783.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  //     img2: "https://images.pexels.com/photos/3951785/pexels-photo-3951785.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //     oldPrice: 58,
+  //     newPrice: 49,
+  //     newItem: true,
+  //   },
+  // ];
+
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      try {
+        const res = await axios.get(
+          import.meta.env.VITE_APP_API_URL + "products?populate=*",
+          {
+            headers: {
+              Authorization: "Bearer " + import.meta.env.VITE_APP_API_TOKEN,
+            },
+          }
+        );
+        setProduct(res.data.data);
+        console.log(res.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchApi();
+  }, []);
 
   return (
     <div className="sections my-24 mx-48">
@@ -58,7 +79,7 @@ const Sections = ({ type }) => {
           </span>
         </div>
         <div className="bottom flex justify-center items-center gap-3">
-          {items.map((item) => (
+          {product?.map((item) => (
             <Card item={item} key={item.id} />
           ))}
         </div>
